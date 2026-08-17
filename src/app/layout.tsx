892ab1b1b5b2323413395a1844e1f9fe17dc7836
lukakh1 <lukakhimshiashvili4@gmail.com>
2026-08-17 15:06:13 +0400

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,6 +96,7 @@ export default function RootLayout({
       <body>
         <script dangerouslySetInnerHTML={{ __html: FONT_GATE }} />
         {children}
+        <Analytics />
       </body>
     </html>
   );
